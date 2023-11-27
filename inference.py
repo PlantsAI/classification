0eb42adbe42ceafafe7617d4e4b096b3f8b1953a
Sajjad Aemmi <sajjadaemmi@gmail.com>
2023-11-27 10:35:10 +0330

    Refactoring inference

--- inference.py
+++ inference.py
@@ -12,19 +12,30 @@ parser.add_argument("--size", default=224, type=int)
 parser.add_argument("--thread", default=4, type=int)
 args = parser.parse_args()
 
-opts = onnxruntime.SessionOptions()
-opts.intra_op_num_threads = args.thread
-opts.inter_op_num_threads = args.thread
-opts.execution_mode = onnxruntime.ExecutionMode.ORT_SEQUENTIAL
-session = onnxruntime.InferenceSession(args.weights, opts, providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
-input_name = session.get_inputs()[0].name
-output_name = session.get_outputs()[0].name
 
-image = cv2.imread(args.input)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-image = cv2.resize(image, (args.size, args.size))
-image = image.transpose(2, 0, 1).astype("float32") / 255.0
-image = np.expand_dims(image, axis=0)
-result = session.run([output_name], {input_name: image})
-print(np.argmax(result))
-print(result)
+def load_model():
+    opts = onnxruntime.SessionOptions()
+    opts.intra_op_num_threads = args.thread
+    opts.inter_op_num_threads = args.thread
+    opts.execution_mode = onnxruntime.ExecutionMode.ORT_SEQUENTIAL
+    session = onnxruntime.InferenceSession(args.weights, opts, providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+    input_name = session.get_inputs()[0].name
+    output_name = session.get_outputs()[0].name
+    return session, input_name, output_name
+
+
+def preprocess(image, size):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (size, size))
+    image = image.transpose(2, 0, 1).astype("float32") / 255.0
+    image = np.expand_dims(image, axis=0)
+    return image
+
+
+if __name__ == "__main__":
+    session, input_name, output_name = load_model()    
+    image = cv2.imread(args.input)
+    image = preprocess(image, args.size)
+    result = session.run([output_name], {input_name: image})
+    print(result)
+    print(np.argmax(result))
